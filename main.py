e835d8eb1200497590a21004b938f6d326eb0ce0
BohdanGunko <bodya0000@gmail.com>
2021-12-12 00:00:21 +0200

    AES working. Code fixes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
 
 
-class encoding_method(Enum):
+class encryption_method(Enum):
     Camellia = 1
     AES = 2
     Kalina = 3
@@ -24,34 +24,40 @@ def remove_padding(data, size=128):
     return unpadded_data
 
 
-def encrypt_camellia(sym_key, text):
+def encrypt_common(cipher, text):
     text = add_padding(text)
-    cipher = Cipher(algorithms.Camellia(sym_key), modes.ECB())
     encryptor = cipher.encryptor()
     return encryptor.update(text) + encryptor.finalize()
 
 
-def decrypt_camellia(sym_key, encrypted_text):
-    cipher = Cipher(algorithms.Camellia(sym_key), modes.ECB())
+def decrypt_common(cipher, encrypted_text):
     decryptor = cipher.decryptor()
     text = decryptor.update(encrypted_text) + decryptor.finalize()
     return remove_padding(text)
 
 
 def encrypt_text(enc_method, sym_key, text):
-    if enc_method == encoding_method.Camellia:
-        return encrypt_camellia(sym_key, text)
+    if enc_method == encryption_method.Camellia:
+        cipher = Cipher(algorithms.Camellia(sym_key), modes.ECB())
+        return encrypt_common(cipher, text)
+    elif enc_method == encryption_method.AES:
+        cipher = Cipher(algorithms.AES(sym_key), modes.ECB())
+        return encrypt_common(cipher, text)
 
 
 def decrypt_text(enc_method, sym_key, encrypted_text):
-    if enc_method == encoding_method.Camellia:
-        return decrypt_camellia(sym_key, encrypted_text)
+    if enc_method == encryption_method.Camellia:
+        cipher = Cipher(algorithms.Camellia(sym_key), modes.ECB())
+        return decrypt_common(cipher, encrypted_text)
+    elif enc_method == encryption_method.AES:
+        cipher = Cipher(algorithms.AES(sym_key), modes.ECB())
+        return decrypt_common(cipher, encrypted_text)
 
 
 if __name__ == '__main__':
-    enc_method = encoding_method.Camellia
+    enc_method = encryption_method.AES
     sym_key = "1234567890abcdef".encode("utf-8")
-    text = "dsasdadasdsadsad".encode("utf-8")
+    text = "dsasdadasdsadsad123".encode("utf-8")
 
     encrypted_text = encrypt_text(enc_method, sym_key, text)
     decoded_text = decrypt_text(enc_method, sym_key, encrypted_text)
