297be1b64dc6964b775486353c4cdd889848fb71
BohdanGunko <bodya0000@gmail.com>
2021-12-12 17:52:09 +0200

    rsa cipher working

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
 from kalyna_cipher.classEncryption import classEncryption
 from kalyna_cipher.classDecryption import classDecryption
-from kalyna_cipher.classRound import classRound
 from kalyna_cipher.classBasic import classBasic
-from kalyna_cipher.classKey import classKey
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
 
 class encryption_method(Enum):
     Camellia = 1
     AES = 2
     Kalyna = 3
+
+
+def get_rsa_key(filepath):
+    return open(filepath, 'r').read()
 
 
 def add_padding(data, size=128):
@@ -79,6 +83,18 @@ def decrypt_kalyna(sym_key, encrypted_text):
     return remove_padding(text).decode("utf-8")
 
 
+def rsa_encrypt(rsa_pub_key, text):
+    rsa_pub_key = RSA.importKey(rsa_pub_key)
+    rsa_pub_key = PKCS1_OAEP.new(rsa_pub_key)
+    return rsa_pub_key.encrypt(text)
+
+
+def rsa_decrypt(rsa_priv_key, encrypted_text):
+    rsa_priv_key = RSA.importKey(rsa_priv_key)
+    rsa_priv_key = PKCS1_OAEP.new(rsa_priv_key)
+    return rsa_priv_key.decrypt(encrypted_text)
+
+
 def encrypt_text(enc_method, sym_key, text):
     if enc_method == encryption_method.Camellia:
         cipher = Cipher(algorithms.Camellia(sym_key), modes.ECB())
@@ -101,13 +117,45 @@ def decrypt_text(enc_method, sym_key, encrypted_text):
         return decrypt_kalyna(sym_key, encrypted_text)
 
 
-if __name__ == '__main__':
-    enc_method = encryption_method.Camellia
-    sym_key = "1234567890abcdef".encode("utf-8")
-    text = "1234567890abcdef1234567890abcdef1234567890abcdef".encode("utf-8")
+def create_final_msg(enc_method, encrypted_sym_key, encrypted_text):
+    enc_method = enc_method.value.to_bytes(1, byteorder='little')
+    return enc_method + encrypted_sym_key + encrypted_text
+
+
+def split_encrypted_msg(msg):
+    enc_method = msg[:1]
+    encrypted_sym_key = msg[1:513]
+    encrypted_text = msg[513:]
+    return enc_method, encrypted_sym_key, encrypted_text
+
+
+def encrypt(rsa_pub_key, enc_method, sym_key, text):
+    sym_key = sym_key.encode("utf-8")
+    text = text.encode("utf-8")
 
     encrypted_text = encrypt_text(enc_method, sym_key, text)
-    decoded_text = decrypt_text(enc_method, sym_key, encrypted_text)
+    encrypted_sym_key = rsa_encrypt(rsa_pub_key, sym_key)
+    return create_final_msg(enc_method, encrypted_sym_key, encrypted_text)
 
-    print(encrypted_text)
-    print(decoded_text)
+
+def decrypt(rsa_priv_key, msg):
+    enc_method, encrypted_sym_key, encrypted_text = split_encrypted_msg(msg)
+    sym_key = rsa_decrypt(rsa_priv_key, encrypted_sym_key)
+    enc_method = int.from_bytes(enc_method, "little")
+    enc_method = encryption_method(enc_method)
+
+    res = decrypt_text(enc_method, sym_key, encrypted_text)
+    return res
+
+
+if __name__ == '__main__':
+    enc_method = encryption_method.Kalyna
+    sym_key = "1234567890abcdef"
+    text = "1234567890abcdef1234567890abcdef1234567890abcdef"
+
+    rsa_pub_key = get_rsa_key("dummy_keys/rsa.pem.pub")
+    rsa_priv_key = get_rsa_key("dummy_keys/rsa.pem")
+
+    encrypted_msg = encrypt(rsa_pub_key, enc_method, sym_key, text)
+    decrypted_msg = decrypt(rsa_priv_key, encrypted_msg)
+    print(decrypted_msg)
