0093795ebfd662b23ad7caefdf6f7cb22a203867
BohdanGunko <bodya0000@gmail.com>
2021-12-12 02:08:13 +0200

    Kalyna cipher working

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from enum import Enum
-import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
+from kalyna_cipher.classEncryption import classEncryption
+from kalyna_cipher.classDecryption import classDecryption
+from kalyna_cipher.classRound import classRound
+from kalyna_cipher.classBasic import classBasic
+from kalyna_cipher.classKey import classKey
 
 
 class encryption_method(Enum):
@@ -24,6 +28,14 @@ def remove_padding(data, size=128):
     return unpadded_data
 
 
+def from_bytes_to_hex_string(bytes_arr):
+    return "".join("{:02x}".format(byte) for byte in bytes_arr)
+
+
+def from_hex_string_to_bytes(string):
+    return bytearray.fromhex(string)
+
+
 def encrypt_common(cipher, text):
     text = add_padding(text)
     encryptor = cipher.encryptor()
@@ -33,7 +45,38 @@ def encrypt_common(cipher, text):
 def decrypt_common(cipher, encrypted_text):
     decryptor = cipher.decryptor()
     text = decryptor.update(encrypted_text) + decryptor.finalize()
-    return remove_padding(text)
+    return remove_padding(text).decode("utf-8")
+
+
+def encrypt_kalyna(sym_key, text):
+    sym_key = from_bytes_to_hex_string(sym_key)
+    text = add_padding(text)
+    text = from_bytes_to_hex_string(text)
+    converter = classBasic()
+    encryptor = classEncryption(False)
+
+    encrypted_text = ""
+    for i in range(0, int(len(text)/32)):
+        text_chunk = text[i * 32: i * 32 + 32]
+        encrypted_text_chunk = encryptor.func_encrypt(
+            converter.func_string_to_mas(text_chunk), converter.func_string_to_mas(sym_key))
+        encrypted_text += converter.func_matrix_to_string(encrypted_text_chunk)
+    return encrypted_text.encode("utf-8")
+
+
+def decrypt_kalyna(sym_key, encrypted_text):
+    sym_key = from_bytes_to_hex_string(sym_key)
+    converter = classBasic()
+    decryptor = classDecryption(False)
+
+    text = ""
+    for i in range(0, int(len(encrypted_text)/32)):
+        encrypted_text_chunk = encrypted_text[i * 32: i * 32 + 32]
+        text_chunk = decryptor.func_decrypt(
+            converter.func_string_to_mas(encrypted_text_chunk.decode("utf-8")), converter.func_string_to_mas(sym_key))
+        text += converter.func_matrix_to_string(text_chunk)
+    text = from_hex_string_to_bytes(text)
+    return remove_padding(text).decode("utf-8")
 
 
 def encrypt_text(enc_method, sym_key, text):
@@ -43,6 +86,8 @@ def encrypt_text(enc_method, sym_key, text):
     elif enc_method == encryption_method.AES:
         cipher = Cipher(algorithms.AES(sym_key), modes.ECB())
         return encrypt_common(cipher, text)
+    elif enc_method == encryption_method.Kalyna:
+        return encrypt_kalyna(sym_key, text)
 
 
 def decrypt_text(enc_method, sym_key, encrypted_text):
@@ -52,13 +97,17 @@ def decrypt_text(enc_method, sym_key, encrypted_text):
     elif enc_method == encryption_method.AES:
         cipher = Cipher(algorithms.AES(sym_key), modes.ECB())
         return decrypt_common(cipher, encrypted_text)
+    elif enc_method == encryption_method.Kalyna:
+        return decrypt_kalyna(sym_key, encrypted_text)
 
 
 if __name__ == '__main__':
-    enc_method = encryption_method.AES
+    enc_method = encryption_method.Camellia
     sym_key = "1234567890abcdef".encode("utf-8")
-    text = "dsasdadasdsadsad123".encode("utf-8")
+    text = "1234567890abcdef1234567890abcdef1234567890abcdef".encode("utf-8")
 
     encrypted_text = encrypt_text(enc_method, sym_key, text)
     decoded_text = decrypt_text(enc_method, sym_key, encrypted_text)
+
+    print(encrypted_text)
     print(decoded_text)
